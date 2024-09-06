0539b05966f7f79b354bd71c6dfb825e8524e34f
Taisei <taisei.960420@gmail.com>
2024-09-07 08:23:12 +0900

    セクションのHTMLとCSSを実装

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,16 +1,30 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default function IndexPage() {
   return (
     <>
       <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12">
-        <div className="container text-center flex flex-col items-center gap-4 max-w-[64rem]">
-          <Link href={"/"}>xをフォローする</Link>
-          <h1>Post Writer</h1>
-          <p>
+        <div className="container text-center flex flex-col items-center gap-4 max-w-[64rem] mx-auto">
+          <Link
+            href={"/"}
+            className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm"
+          >xをフォローする</Link>
+          <h1 className="font-extrabold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">Post Writer</h1>
+          <p className="text-muted-foreground sm:text-xl leading-normal max-w-[42rem]">
             このアプリはNext.js
             AppRouterで作られたものです。ユーザーは自由に投稿をポストできます。
           </p>
+          <div className="space-x-4">
+            <Link href={"/login"} className={cn(buttonVariants({ size: "lg" }))}>はじめる</Link>
+            <Link
+              href={"/"}
+              className={cn(buttonVariants({ size: "lg", variant: "outline" }))}
+              target="_blank"
+              rel="noreferrer"
+            >Github</Link>
+          </div>
         </div>
       </section>
     </>
